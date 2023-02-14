0972a306455b7a223f2ce871add7f0b519eb1b5a
conorgibbons <105668608+conorgibbons@users.noreply.github.com>
2023-02-14 13:58:05 -0500

    testing

--- assignments/03-RegOpt/scheduler.py
+++ assignments/03-RegOpt/scheduler.py
@@ -2,7 +2,6 @@ from typing import List
 
 from torch.optim.lr_scheduler import _LRScheduler
 
-import math
 
 
 class CustomLRScheduler(_LRScheduler):
@@ -20,46 +19,23 @@ class CustomLRScheduler(_LRScheduler):
         """
         # ... Your Code Here ...
 
-        self.T_max = kwargs["T_max"]
-        self.eta_min = kwargs["eta_min"]
+        self.gamma = kwargs["gamma"]
+        self.milestones = Counter(kwargs["milestones"])
         super(CustomLRScheduler, self).__init__(optimizer, last_epoch)
 
     def get_lr(self) -> List[float]:
         """
         Test...
         """
-        if self.last_epoch == 0:
-            return [group["lr"] for group in self.optimizer.param_groups]
-        elif self._step_count == 1 and self.last_epoch > 0:
-            return [
-                self.eta_min
-                + (base_lr - self.eta_min)
-                * (1 + math.cos((self.last_epoch) * math.pi / self.T_max))
-                / 2
-                for base_lr, group in zip(self.base_lrs, self.optimizer.param_groups)
-            ]
-        elif (self.last_epoch - 1 - self.T_max) % (2 * self.T_max) == 0:
-            return [
-                group["lr"]
-                + (base_lr - self.eta_min) * (1 - math.cos(math.pi / self.T_max)) / 2
-                for base_lr, group in zip(self.base_lrs, self.optimizer.param_groups)
-            ]
-        return [
-            (1 + math.cos(math.pi * self.last_epoch / self.T_max))
-            / (1 + math.cos(math.pi * (self.last_epoch - 1) / self.T_max))
-            * (group["lr"] - self.eta_min)
-            + self.eta_min
-            for group in self.optimizer.param_groups
-        ]
+        if self.last_epoch not in self.milestones:
+            return [group['lr'] for group in self.optimizer.param_groups]
+        return [group['lr'] * self.gamma ** self.milestones[self.last_epoch]
+                for group in self.optimizer.param_groups]
 
     def _get_closed_form_lr(self):
         """
         Test
         """
-        return [
-            self.eta_min
-            + (base_lr - self.eta_min)
-            * (1 + math.cos(math.pi * self.last_epoch / self.T_max))
-            / 2
-            for base_lr in self.base_lrs
-        ]
+        milestones = list(sorted(self.milestones.elements()))
+        return [base_lr * self.gamma ** bisect_right(milestones, self.last_epoch)
+                for base_lr in self.base_lrs]
