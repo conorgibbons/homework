afe6ca5c3a5ca9cec00d1e8efdd50efa861a6ff1
conorgibbons <105668608+conorgibbons@users.noreply.github.com>
2023-02-19 21:24:29 -0500

    linear lr

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -8,14 +8,15 @@ from torchvision.transforms import Compose, Normalize, ToTensor
 class CONFIG:
     batch_size = 64
     num_epochs = 200
-    initial_learning_rate = 0.001
+    initial_learning_rate = 0.005
     initial_weight_decay = 0
 
     lrs_kwargs = {
         # You can pass arguments to the learning rate scheduler
         # constructor here.
-        "eta_min": 0.000001,
-        "T_max": 10,
+        "start_factor": 0.33,
+        "end_factor": 1,
+        "total_iters": 10,
     }
 
     optimizer_factory: Callable[
--- assignments/03-RegOpt/scheduler.py
+++ assignments/03-RegOpt/scheduler.py
@@ -2,6 +2,7 @@ from typing import List
 
 from torch.optim.lr_scheduler import _LRScheduler
 
+import math
 
 
 class CustomLRScheduler(_LRScheduler):
@@ -19,23 +20,47 @@ class CustomLRScheduler(_LRScheduler):
         """
         # ... Your Code Here ...
 
-        self.gamma = kwargs["gamma"]
-        self.milestones = Counter(kwargs["milestones"])
+        self.start_factor = kwargs["start_factor"]
+        self.end_factor = kwargs["end_factor"]
+        self.total_iters = kwargs["total_iters"]
         super(CustomLRScheduler, self).__init__(optimizer, last_epoch)
 
     def get_lr(self) -> List[float]:
         """
         Test...
         """
-        if self.last_epoch not in self.milestones:
-            return [group['lr'] for group in self.optimizer.param_groups]
-        return [group['lr'] * self.gamma ** self.milestones[self.last_epoch]
-                for group in self.optimizer.param_groups]
+        if self.last_epoch == 0:
+            return [
+                group["lr"] * self.start_factor for group in self.optimizer.param_groups
+            ]
+
+        if self.last_epoch > self.total_iters:
+            return [group["lr"] for group in self.optimizer.param_groups]
+
+        return [
+            group["lr"]
+            * (
+                1.0
+                + (self.end_factor - self.start_factor)
+                / (
+                    self.total_iters * self.start_factor
+                    + (self.last_epoch - 1) * (self.end_factor - self.start_factor)
+                )
+            )
+            for group in self.optimizer.param_groups
+        ]
 
     def _get_closed_form_lr(self):
         """
         Test
         """
-        milestones = list(sorted(self.milestones.elements()))
-        return [base_lr * self.gamma ** bisect_right(milestones, self.last_epoch)
-                for base_lr in self.base_lrs]
+        return [
+            base_lr
+            * (
+                self.start_factor
+                + (self.end_factor - self.start_factor)
+                * min(self.total_iters, self.last_epoch)
+                / self.total_iters
+            )
+            for base_lr in self.base_lrs
+        ]
