4706e1277f3056a82da3ff726ca34e48324ed184
conorgibbons <105668608+conorgibbons@users.noreply.github.com>
2023-02-20 22:29:26 -0500

    batch 32 epochs 10

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -7,7 +7,7 @@ from torchvision.transforms import Compose, Normalize, ToTensor, RandomHorizonta
 
 class CONFIG:
     batch_size = 32
-    num_epochs = 14
+    num_epochs = 10
     initial_learning_rate = 0.0015
     initial_weight_decay = 0
 
