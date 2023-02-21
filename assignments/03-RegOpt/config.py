52e9f32374530ed7d9e4dd7d05433e0cb5909c10
conorgibbons <105668608+conorgibbons@users.noreply.github.com>
2023-02-20 23:18:04 -0500

    batch 32 epochs 8 normed

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -2,20 +2,20 @@ from typing import Callable
 import torch
 import torch.optim
 import torch.nn as nn
-from torchvision.transforms import Compose, Normalize, ToTensor
+from torchvision.transforms import Compose, Normalize, ToTensor, RandomHorizontalFlip
 
 
 class CONFIG:
     batch_size = 32
     num_epochs = 8
-    initial_learning_rate = 0.0015
-    initial_weight_decay = 0.001
+    initial_learning_rate = 0.002
+    initial_weight_decay = 0
 
     lrs_kwargs = {
         # You can pass arguments to the learning rate scheduler
         # constructor here.
         "gamma": 0.99,
-        "step_size": 590,
+        "step_size": 420,
     }
 
     optimizer_factory: Callable[
@@ -30,6 +30,7 @@ class CONFIG:
     transforms = Compose(
         [
             ToTensor(),
-            Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5]),
+            RandomHorizontalFlip(),
+            Normalize([0.4914, 0.4822, 0.4465], [0.247, 0.243, 0.261]),
         ]
     )
